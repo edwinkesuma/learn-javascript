bbf8d34d6b889ccc59c5e8e40897cb1728df47a0
edwinkesuma <edwin.kesuma.EK21@gmail.com>
2023-06-26 22:56:41 +0700

    comment

---
+++ variable_datatype/02-comments&shortcuts/script.js
@@ -0,0 +1,19 @@
+// console.log("Hello world!");
+
+// console.error("Alert");
+
+// console.warn("Warning");
+
+// console.table({ name: "edwin", email: "edwinkesuma@mail.com" });
+
+// const x = 100;
+
+// console.group("simple");
+// console.log(x);
+// console.error("Alert");
+// console.warn("Warning");
+// console.groupEnd();
+
+// const styles = "padding: 10px; background: white; color: green;";
+
+// console.log("%cHello World", styles);
